a0571eedc34084a441afed10dfb4d76021f0b071
Nikita Nikitin <nioktn@gmail.com>
2018-09-06 16:41:04 +0300

    First simple test

--- Tests/TestOne.cs
+++ Tests/TestOne.cs
@@ -9,9 +9,30 @@ namespace Tests
     [TestFixture]
     public class TestOne
     {
+        IWebDriver driver;
+        ChromeOptions options;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
+
         [Test]
         public void TestMethod1()
         {
+            driver.Url = "https://www.google.com/";
+            IWebElement searchField = driver.FindElement(By.Id("lst-ib"));
+            searchField.SendKeys("hi there");
+            searchField.SendKeys(Keys.Enter);
+        }
+
+        [OneTimeTearDown]
+        public void LastCleanUp()
+        {
+            driver.Quit();
         }
     }
 }
