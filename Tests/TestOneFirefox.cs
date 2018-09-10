4837d4b9dd7a6846bc1dc64990c98dcf169212cd
Nikita Nikitin <nniki@softserveinc.com>
2018-09-10 10:32:51 +0300

    Added firefox test

--- Tests/TestOneFirefox.cs
+++ Tests/TestOneFirefox.cs
@@ -3,6 +3,7 @@ using NUnit;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using Pages;
 
 namespace Tests
 {
@@ -12,10 +13,17 @@ namespace Tests
         [Test]
         public void TestMethod1()
         {
-            driver.Url = "https://www.google.com/";
-            IWebElement searchField = driver.FindElement(By.Id("lst-ib"));
-            searchField.SendKeys("hi there");
-            searchField.SendKeys(Keys.Enter);
+            driver.Url = "http://localhost/litecart/en/";
+            LoginSection ls = new LoginSection(driver);
+            ls.LogInStoreUser("test@gmail.com", "testpassword1");
+        }
+
+        [Test]
+        public void EnterAdminPage()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            LoginSection loginSection = new LoginSection(driver);
+            loginSection.LogInAdminPage("admin", "admin");
         }
     }
 }
