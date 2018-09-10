e7e710c72800c77a66b8f319348c80b98c848c6e
Nikita Nikitin <nioktn@gmail.com>
2018-09-10 16:48:54 +0300

    Added sticker test for diff browsers

---
+++ Tests/Firefox/MainPageStickers.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using Pages;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Tests.Firefox
+
+{
+    [TestFixture]
+    public class MainPageStickers : BaseTest<FirefoxDriver>
+    {
+        [Test]
+        public void AllProductsStickerPresence()
+        {
+            driver.Url = "http://localhost/litecart/";
+            MainPage mainPage = new MainPage(driver);
+
+            List<bool> stickersPresenceResults = new List<bool>();
+            var productsList = mainPage.GetAllProducts(wait);
+            foreach (var item in productsList)
+            {
+                stickersPresenceResults.Add(new ProductCompactView(driver, item).HasOneSticker(wait));
+                Console.WriteLine(new ProductCompactView(driver, item).HasOneSticker(wait));
+            }
+
+            int testResult = 1;
+            foreach (var item in stickersPresenceResults)
+            {
+                testResult *= Convert.ToInt32(item);
+            }
+            Assert.IsTrue(testResult == 1);
+        }
+    }
+}
