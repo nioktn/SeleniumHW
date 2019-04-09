01f090d639c3f736fd6aaac70f07eb3aa9fc2d30
Nikita Nikitin <nioktn@gmail.com>
2019-04-09 11:13:10 +0300

    commit message

---
+++ Pages/Models/NewCustomerModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Pages.Models
+{
+    public class NewCustomerModel
+    {
+        public string TaxId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Postcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Newsletter { get; set; }
+        public string DesiredPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Pages/PageObjects/NavigationMenu/LoggedUserSection.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Pages.PageObjects.NavigationMenu
+{
+    public class LoggedUserSection : Navigation
+    {
+        public LoggedUserSection(IWebDriver webDriver) : base (webDriver) { }
+
+        private readonly By _loggedUserSection = By.CssSelector("#box-account");
+        private readonly By _customerService = By.XPath("//div[@id='box-account']/.//a[contains(text(), 'Customer Service')]");
+        private readonly By _orderHistory = By.XPath("//div[@id='box-account']/.//a[contains(text(), 'Order History')]");
+        private readonly By _editAccount = By.XPath("//div[@id='box-account']/.//a[contains(text(), 'Edit Account')]");
+        private readonly By _logout = By.XPath("//div[@id='box-account']/.//a[contains(text(), 'Logout')]");
+
+        public IWebElement CustomerService { get => webDriver.FindElement(_customerService); }
+        public IWebElement OrderHistory { get => webDriver.FindElement(_orderHistory); }
+        public IWebElement EditAccout { get => webDriver.FindElement(_editAccount); }
+        public IWebElement Logout { get => webDriver.FindElement(_logout); }
+
+
+        public void LogOut()
+        {
+            Wait.GetInstance(webDriver, TimeSpan.FromSeconds(10)).Until((d) => ElemHelper.IsElementVisible(webDriver, _logout));
+            Logout.Click();
+        }
+    }
+}
